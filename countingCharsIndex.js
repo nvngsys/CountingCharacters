c9303ba600016345e8980b169588084561034d3d
Jack Brennan <jack.brennan@nvngsystems.com>
2019-05-01 20:27:23 +0000

    add new counting char that show index found

---
+++ countingCharsIndex.js
@@ -0,0 +1,22 @@
+countLetters = function (str) {
+    str = str.replace(/\s/g, '');
+    var objResult = {}; 
+    var arrString = str.split(''); 
+    if (arrString instanceof Array) {
+        arrString.forEach(function (v, i) {
+            if (!objResult[v]) {
+                objResult[v] = [i];
+            } else {
+                objResult[v].push(i);
+            }
+        })
+
+        // Object.keys(objResult).forEach(function (v) {
+        //     objResult[v] = objResult[v].length;
+        // });
+
+    }
+    console.log(objResult);
+    return objResult;
+}
+countLetters("lighthouse in the house") 
